857dfe1b04bb6236d877ef30e70e1513dda1cd1c
Adolfo <adolfodelarosa2012@gmail.com>
2019-08-20 09:00:04 +0200

    Leyendo Categorías de Eventos desde el REST API y mostrarlas en el Select

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -6,6 +6,13 @@ class EventBrite {
     //Obtiene las categorias en init()
     async obtenerCategorias(){
         //Consultar las categorias a la REST API de vent brite
+        const respuestaCategorias = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.token_auth}`);
+
+        //Esperar la respuesta de las categorias y devolver un JSON
+        const categorias = await respuestaCategorias.json();
+
+        //Devolver el resultado
+        return { categorias }
 
     }
 }
--- js/interfaz.js
+++ js/interfaz.js
@@ -7,10 +7,26 @@ class Interfaz {
     }
     //Método para cuando se inicialice la app
     init(){
-
+        //Llamar a imprimir categorias de la REST API
+        this.imprimirCategorias();
     }
     //Imprimir categorias
     imprimirCategorias(){
-        const listaCategorias = eventbrite.obtenerCategorias();
+        const listaCategorias = eventbrite.obtenerCategorias()
+            .then( categorias => {
+                //console.log( categorias );
+                //console.log( categorias.categorias.categories );
+                const cats = categorias.categorias.categories;
+                //Seleccionar el select de categorias
+                const selectCategoria = document.getElementById('listado-categorias');
+
+                //Recorremos el arreglo e imprimimos los <option>
+                cats.forEach( cat => {
+                    const option = document.createElement('option');
+                    option.value = cat.id;
+                    option.appendChild(document.createTextNode(cat.name_localized));
+                    selectCategoria.appendChild(option);
+                });
+            });
     }
 }
